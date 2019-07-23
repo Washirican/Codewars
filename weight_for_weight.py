a5d470b3fad432ee2241cbbbec6f4f7a73127c23
Washirican <danrod@uw.edu>
2019-07-22 20:53:20 -0700

    Weight for weight Kata update.

--- weight_for_weight.py
+++ weight_for_weight.py
@@ -1,4 +1,8 @@
 # Weight for weight Codewars Kata
+# Example:
+# "56 65 74 100 99 68 86 180 90" ordered by numbers weights
+# becomes: "100 180 90 56 65 74 68 86 99"
+
 
 def order_weight(strng):
     # your code
